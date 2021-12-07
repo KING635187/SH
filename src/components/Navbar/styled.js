4b699fcb903b02496b89210458ac79612f127cc3
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-12-07 15:34:26 +0500

    Remove cursor pointer on music-start button

--- src/components/Navbar/styled.js
+++ src/components/Navbar/styled.js
@@ -1,41 +1,40 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const StyledComponent = styled.div`
-  width: 100%;
-  position: fixed;
-  z-index: 2;
-`;
+	width: 100%;
+	position: fixed;
+	z-index: 2;
+`
 
 export const StyledLogo = styled.h2`
-  font-family: Blanka;
-  font-size: 36px;
-  line-height: 43px;
-  color: #2ac7bd;
-  padding-top: 12px;
-  display: inline-block;
-  position: absolute;
-  right: 0;
-  margin-right: 20px;
+	font-family: Blanka;
+	font-size: 36px;
+	line-height: 43px;
+	color: #2ac7bd;
+	padding-top: 12px;
+	display: inline-block;
+	position: absolute;
+	right: 0;
+	margin-right: 20px;
 
-  @media (max-width: 1360px) {
-    font-size: 28px;
-    padding-top: 6px;
-  }
-`;
+	@media (max-width: 1360px) {
+		font-size: 28px;
+		padding-top: 6px;
+	}
+`
 
 export const StyledMusic = styled.button`
-  font-family: Blanka;
-  color: #2ac7bd;
-  margin-right: 164px;
-  position: absolute;
-  right: 0;
-  margin-top: 30px;
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
+	font-family: Blanka;
+	color: #2ac7bd;
+	margin-right: 164px;
+	position: absolute;
+	right: 0;
+	margin-top: 30px;
+	border: none;
+	background-color: transparent;
 
-  @media (max-width: 1360px) {
-    font-size: 10px;
-    margin-top: 20px;
-  }
-`;
+	@media (max-width: 1360px) {
+		font-size: 10px;
+		margin-top: 20px;
+	}
+`
